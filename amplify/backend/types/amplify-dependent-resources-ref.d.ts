16eb59d300159c4f8293fe785f75e5a4ae5537dc
Tanya Sweeney <tanyalsweeney@yahoo.com>
2023-03-29 15:34:53 -0700

    Add graphql

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyreactgraphql": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "amplifyreactgraphql9fe8155f": {
       "AppClientID": "string",
